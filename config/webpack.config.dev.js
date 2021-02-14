acb1011e5e4e1f7532cd965a04c1befccf55999a
ATQQ <2604395430@qq.com>
2021-02-14 19:46:13 +0800

    feat: 完成第一个静态页简历编写

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -7,7 +7,5 @@ module.exports = {
         host: 'localhost', //服务器ip地址
         port: 8088, //端口
         open: true, //自动打开页面
-        hot: true, //模块热替换
-        hotOnly: false, //只有热更新不会刷新页面
     }
 }
