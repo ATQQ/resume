3a382801d755b01eb6ef5d7db91de2ef5bb25194
ATQQ <2604395430@qq.com>
2021-02-26 16:08:26 +0800

    chore: 模板样式微调

--- src/constants/schema.js
+++ src/constants/schema.js
@@ -1,6 +1,7 @@
 import demo1 from './schema/demo1'
 import demo2 from './schema/demo2'
+import demo3 from './schema/demo3'
 
 export default{
-    demo1,demo2
+    demo1,demo2,demo3
 }
---
+++ src/constants/schema/demo3.js
@@ -0,0 +1,10 @@
+export default {
+    name: 'React编写',
+    position: 'Web前端工程师',
+    dataString: [
+        '文字内容1',
+        '文字内容2',
+        '文字内容3',
+        '文字内容4',
+    ]
+}
